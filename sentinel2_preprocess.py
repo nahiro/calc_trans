a394866eb0eff3e913f48e2c779c172db1213fa0
Naohiro Manago <manago.naohiro@gmail.com>
2022-09-12 05:45:08 +0900

    NM modified files.

--- sentinel2_preprocess.py
+++ sentinel2_preprocess.py
@@ -9,16 +9,21 @@ from argparse import ArgumentParser,RawTextHelpFormatter
 HOME = os.environ.get('HOME')
 if HOME is None:
     HOME = os.environ.get('USERPROFILE')
+TOPDIR = os.path.join(HOME,'Work')
 
 # Default values
 SCRDIR = os.path.join(HOME,'SatelliteTool')
+S2_DATA = os.path.join(TOPDIR,'Sentinel-2_Data')
+GIS_FNAM = os.path.join(TOPDIR,'Shapefile','All_area_polygon_20210914','All_area_polygon_20210914.shp')
 TMAX = datetime.now().strftime('%Y%m%d')
 DATA_TMAX = TMAX
 TMGN = 30
 
 # Read options
 parser = ArgumentParser(formatter_class=lambda prog:RawTextHelpFormatter(prog,max_help_position=200,width=200))
-parser.add_argument('--scrdir',default=SCRDIR,help='Script directory (%(default)s)')
+parser.add_argument('--scrdir',default=SCRDIR,help='Script folder (%(default)s)')
+parser.add_argument('--s2_data',default=S2_DATA,help='Sentinel-2 data folder (%(default)s)')
+parser.add_argument('--gis_fnam',default=GIS_FNAM,help='Parcel data (%(default)s)')
 parser.add_argument('-s','--tmin',default=None,help='Min date for analysis in the format YYYYMMDD (%(default)s)')
 parser.add_argument('-e','--tmax',default=TMAX,help='Max date for analysis in the format YYYYMMDD (%(default)s)')
 parser.add_argument('--data_tmin',default=None,help='Min date of input data in the format YYYYMMDD (%(default)s)')
@@ -26,24 +31,20 @@ parser.add_argument('--data_tmax',default=DATA_TMAX,help='Max date of input data
 parser.add_argument('--tmgn',default=TMGN,type=float,help='Margin of input data in day (%(default)s)')
 args = parser.parse_args()
 
+s2_analysis = 
+
 tmp_fnam = 'temp'
 with open(tmp_fnam) as fp:
     fp.write('[DEFAULT]\n')
-    fp.write('HOME                                = %(USERPROFILE)s\n')
-    fp.write('top_dir                             = %(HOME)s/Work\n')
-    fp.write('scr_dir                             = %(HOME)s/SatelliteTool\n')
-    fp.write('s1_data                             = %(top_dir)s/Sentinel-1_Data\n')
-    fp.write('s1_analysis                         = %(top_dir)s/Sentinel-1_Analysis/Current\n')
-    fp.write('s2_data                             = %(top_dir)s/Sentinel-2_Data\n')
-    fp.write('s2_analysis                         = %(top_dir)s/Sentinel-2_Analysis/Current\n')
-    fp.write('gis_fnam                            = %(top_dir)s/Shapefile/All_area_polygon_20210914/All_area_polygon_20210914.shp\n')
+    fp.write('scr_dir                             = {}\n'.format(args.scrdir))
+    fp.write('s2_data                             = {}\n'.format(args.s2_data))
+    fp.write('gis_fnam                            = {}\n'.format(args.gis_fnam))
     fp.write('[main]\n')
     fp.write('#main.start_date                     =\n')
     fp.write('#main.end_date                       =\n')
     fp.write('main.first_date                     =\n')
     fp.write('main.last_date                      =\n')
-    fp.write('main.s2_data                        = %(s2_data)s\n')
-    fp.write('main.s2_analysis                    = %(s2_analysis)s\n')
+    fp.write('main.s2_data                        = {}\n'.format(args.s2_data))
     fp.write('main.download                       = False\n')
     fp.write('main.geocor                         = True\n')
     fp.write('main.parcel                         = True\n')
@@ -54,3 +55,5 @@ with open(tmp_fnam) as fp:
     fp.write('main.formula                        = False\n')
     fp.write('main.estimate                       = False\n')
     fp.write('main.no_gui                         = True\n')
+
+for dnam in soreted()
