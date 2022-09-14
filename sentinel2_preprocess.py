a5fa504058ae78e232ab592be0bc9c2ca9713858
Naohiro Manago <manago.naohiro@gmail.com>
2022-09-15 06:07:32 +0900

    NM modified files.

--- sentinel2_preprocess.py
+++ sentinel2_preprocess.py
@@ -12,6 +12,7 @@ if HOME is None:
 TOPDIR = os.path.join(HOME,'Work')
 
 # Default values
+SITE = 'Cihea'
 SCRDIR = os.path.join(HOME,'SatelliteTool')
 S2_DATA = os.path.join(TOPDIR,'Sentinel-2_Data')
 GIS_FNAM = os.path.join(TOPDIR,'Shapefile','All_area_polygon_20210914','All_area_polygon_20210914.shp')
@@ -22,6 +23,7 @@ TMGN = 30
 
 # Read options
 parser = ArgumentParser(formatter_class=lambda prog:RawTextHelpFormatter(prog,max_help_position=200,width=200))
+parser.add_argument('-S','--site',default=SITE,help='Site name (%(default)s)')
 parser.add_argument('--scrdir',default=SCRDIR,help='Script folder (%(default)s)')
 parser.add_argument('--s2_data',default=S2_DATA,help='Sentinel-2 data folder (%(default)s)')
 parser.add_argument('--gis_fnam',default=GIS_FNAM,help='Parcel data file (%(default)s)')
@@ -42,18 +44,24 @@ tmax = datetime.strptime(args.tmax,'%Y%m%d')
 d1 = datetime.strptime(args.data_tmin,'%Y%m%d')
 d2 = datetime.strptime(args.data_tmax,'%Y%m%d')
 data_years = np.arange(d1.year,d2.year+1,1)
+site_low = args.site.lower()
+if site_low == 'none':
+    s2_data = args.s2_data
+else:
+    s2_data = os.path.join(args.s2_data,args.site)
 
-with tempfile.TemporaryFile(suffix='.ini') as fp:
+#with tempfile.TemporaryFile(suffix='.ini') as fp:
+with open('test.ini','w') as fp:
     fp.write('[DEFAULT]\n')
     fp.write('scr_dir                             = {}\n'.format(args.scrdir))
-    fp.write('s2_data                             = {}\n'.format(args.s2_data))
+    fp.write('s2_data                             = {}\n'.format(s2_data))
     fp.write('gis_fnam                            = {}\n'.format(args.gis_fnam))
     fp.write('[main]\n')
     fp.write('#main.start_date                     =\n')
     fp.write('#main.end_date                       =\n')
     fp.write('main.first_date                     =\n')
     fp.write('main.last_date                      =\n')
-    fp.write('main.s2_data                        = {}\n'.format(args.s2_data))
+    fp.write('main.s2_data                        = {}\n'.format(s2_data))
     fp.write('main.download                       = False\n')
     if args.skip_geocor:
         fp.write('main.geocor                         = False\n')
@@ -78,18 +86,18 @@ with tempfile.TemporaryFile(suffix='.ini') as fp:
     fp.write('main.no_gui                         = True\n')
     fp.write('[geocor]\n')
     fp.write('geocor.ref_fnam                     = {}\n'.format(args.wv_fnam))
-    fp.write('geocor.band_fnam                    = {}\n'.format(os.path.join(args.s2_data,'band_names.txt')))
+    fp.write('geocor.band_fnam                    = {}\n'.format(os.path.join(s2_data,'band_names.txt')))
     fp.write('#geocor.python_path                  = {}\n'.format(args.python_path))
     fp.write('geocor.scr_dir                      = {}\n'.format(args.scrdir))
     fp.write('[parcel]\n')
     fp.write('parcel.gis_fnam                     = {}\n'.format(args.gis_fnam))
-    fp.write('parcel.mask_parcel                  = {}\n'.format(os.path.join(args.s2_data,'parcel_mask.tif')))
+    fp.write('parcel.mask_parcel                  = {}\n'.format(os.path.join(s2_data,'parcel_mask.tif')))
     fp.write('#parcel.python_path                  = {}\n'.format(args.python_path))
     fp.write('parcel.scr_dir                      = {}\n'.format(args.scrdir))
     fp.write('[atcor]\n')
     fp.write('atcor.gis_fnam                      = {}\n'.format(args.gis_fnam))
-    fp.write('atcor.mask_studyarea                = {}\n'.format(os.path.join(args.s2_data,'studyarea_mask.tif')))
-    fp.write('atcor.mask_parcel                   = {}\n'.format(os.path.join(args.s2_data,'parcel_mask.tif')))
+    fp.write('atcor.mask_studyarea                = {}\n'.format(os.path.join(s2_data,'studyarea_mask.tif')))
+    fp.write('atcor.mask_parcel                   = {}\n'.format(os.path.join(s2_data,'parcel_mask.tif')))
     fp.write('#atcor.stat_fnam                     =\n')
     fp.write('#atcor.inds_fnam                     =\n')
     fp.write('#atcor.csv_flag                      = True\n')
@@ -103,7 +111,7 @@ with tempfile.TemporaryFile(suffix='.ini') as fp:
     fp.write('interp.scr_dir                      = {}\n'.format(args.scrdir))
 
 if not args.skip_geocor:
-    geocor_dnam = os.path.join(args.s2_data,'geocor')
+    geocor_dnam = os.path.join(s2_data,'geocor')
     for ystr in soreted(os.listdir(geocor_dnam)):
         if not re.search('^\d\d\d\d$',ystr):
             continue
