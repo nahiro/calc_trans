f033d706202f6563e9589b37695f6647b8f64c84
Naohiro Manago <manago.naohiro@gmail.com>
2022-09-15 06:29:25 +0900

    NM modified files.

--- sentinel2_preprocess.py
+++ sentinel2_preprocess.py
@@ -2,7 +2,8 @@ import os
 import sys
 import re
 import tempfile
-from datetime import datetime
+from datetime import datetime,timedelta
+import numpy as np
 from argparse import ArgumentParser,RawTextHelpFormatter
 
 # Constants
@@ -13,42 +14,53 @@ TOPDIR = os.path.join(HOME,'Work')
 
 # Default values
 SITE = 'Cihea'
+PYTHON_PATH = os.path.join(HOME,'miniconda3','python')
 SCRDIR = os.path.join(HOME,'SatelliteTool')
 S2_DATA = os.path.join(TOPDIR,'Sentinel-2_Data')
 GIS_FNAM = os.path.join(TOPDIR,'Shapefile','All_area_polygon_20210914','All_area_polygon_20210914.shp')
 WV_FNAM = os.path.join(TOPDIR,'WorldView','wv2_180629_mul.tif')
 TMAX = datetime.now().strftime('%Y%m%d')
 DATA_TMAX = TMAX
-TMGN = 30
+GROW_PERIOD = 120 # day
+TMGN = 90 # day
+TSND = 30 # day
 
 # Read options
 parser = ArgumentParser(formatter_class=lambda prog:RawTextHelpFormatter(prog,max_help_position=200,width=200))
 parser.add_argument('-S','--site',default=SITE,help='Site name (%(default)s)')
+parser.add_argument('--python_path',default=PYTHON_PATH,help='Path to the Python (%(default)s)')
 parser.add_argument('--scrdir',default=SCRDIR,help='Script folder (%(default)s)')
 parser.add_argument('--s2_data',default=S2_DATA,help='Sentinel-2 data folder (%(default)s)')
 parser.add_argument('--gis_fnam',default=GIS_FNAM,help='Parcel data file (%(default)s)')
 parser.add_argument('--wv_fnam',default=WV_FNAM,help='WorldView data file (%(default)s)')
-parser.add_argument('-s','--tmin',default=None,help='Min date for analysis in the format YYYYMMDD (%(default)s)')
-parser.add_argument('-e','--tmax',default=TMAX,help='Max date for analysis in the format YYYYMMDD (%(default)s)')
-parser.add_argument('--data_tmin',default=None,help='Min date of input data in the format YYYYMMDD (%(default)s)')
-parser.add_argument('--data_tmax',default=DATA_TMAX,help='Max date of input data in the format YYYYMMDD (%(default)s)')
-parser.add_argument('--tmgn',default=TMGN,type=float,help='Margin of input data in day (%(default)s)')
+parser.add_argument('-s','--tmin',default=None,help='Min date to send in the format YYYYMMDD (%(default)s)')
+parser.add_argument('-e','--tmax',default=TMAX,help='Max date to send in the format YYYYMMDD (%(default)s)')
+parser.add_argument('--data_tmin',default=None,help='Min date to calculate in the format YYYYMMDD (%(default)s)')
+parser.add_argument('--data_tmax',default=DATA_TMAX,help='Max date to calculate in the format YYYYMMDD (%(default)s)')
+parser.add_argument('--grow_period',default=GROW_PERIOD,type=int,help='Length of growing period in day (%(default)s)')
+parser.add_argument('--tmgn',default=TMGN,type=int,help='Margin of calculation period in day (%(default)s)')
+parser.add_argument('--tsnd',default=TSND,type=int,help='Duration of data to send in day (%(default)s)')
 parser.add_argument('--skip_geocor',default=False,action='store_true',help='Skip geocor (%(default)s)')
 parser.add_argument('--skip_parcel',default=False,action='store_true',help='Skip parcel (%(default)s)')
 parser.add_argument('--skip_atcor',default=False,action='store_true',help='Skip atcor (%(default)s)')
 parser.add_argument('--skip_interp',default=False,action='store_true',help='Skip interp (%(default)s)')
 args = parser.parse_args()
-
-tmin = datetime.strptime(args.tmin,'%Y%m%d')
-tmax = datetime.strptime(args.tmax,'%Y%m%d')
-d1 = datetime.strptime(args.data_tmin,'%Y%m%d')
-d2 = datetime.strptime(args.data_tmax,'%Y%m%d')
-data_years = np.arange(d1.year,d2.year+1,1)
 site_low = args.site.lower()
 if site_low == 'none':
     s2_data = args.s2_data
 else:
     s2_data = os.path.join(args.s2_data,args.site)
+tmax = datetime.strptime(args.tmax,'%Y%m%d')
+d2 = datetime.strptime(args.data_tmax,'%Y%m%d')
+if args.tmin is not None:
+    tmin = datetime.strptime(args.tmin,'%Y%m%d')
+else:
+    tmin = tmax-timedelta(days=args.tsnd)
+if args.data_tmin is not None:
+    d1 = datetime.strptime(args.data_tmin,'%Y%m%d')
+else:
+    d1 = d2-timedelta(days=(args.grow_period*2+args.tmgn*2))
+data_years = np.arange(d1.year,d2.year+1,1)
 
 #with tempfile.TemporaryFile(suffix='.ini') as fp:
 with open('test.ini','w') as fp:
@@ -112,7 +124,10 @@ with open('test.ini','w') as fp:
 
 if not args.skip_geocor:
     geocor_dnam = os.path.join(s2_data,'geocor')
-    for ystr in soreted(os.listdir(geocor_dnam)):
-        if not re.search('^\d\d\d\d$',ystr):
-            continue
-        year = int(ystr)
+    if not os.path.isdir(geocor_dnam):
+        pass
+    else:
+        for ystr in sorted(os.listdir(geocor_dnam)):
+            if not re.search('^\d\d\d\d$',ystr):
+                continue
+            year = int(ystr)
