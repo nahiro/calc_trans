9787cf9f389d1298b2f6e56b34e9f5b60e34ca7c
Naohiro Manago <manago.naohiro@gmail.com>
2022-09-16 05:16:28 +0900

    NM modified files.

--- satreps_automation.py
+++ satreps_automation.py
@@ -17,4 +17,26 @@ if len(pids) > 1:
     sys.stderr.flush()
     sys.exit()
 
-time.sleep(1000)
+HOME = os.environ.get('USERPROFILE')
+if HOME is None:
+    HOME = os.environ.get('HOME')
+TOPDIR = 'F:\Work'
+
+python_path = os.path.join(HOME,'miniconda3','python.exe')
+cmddir = os.path.join(HOME,'Automation')
+scrdir = os.path.join(HOME,'SatelliteTool')
+
+for site in ['Cihea','Bojongsoang','Testsite']:
+    # Download/Upload GRD, Calculate/Upload Planting
+    if site != 'Testsite':
+        s1_data = os.path.join(TOPDIR,'Sentinel-1_Data')
+        command = python_path
+        command += ' "{}"'.format(os.path.join(cmddir,'auto_calc_trans.py'))
+        command += ' --datdir "{}"'.format(s1_data)
+        command += ' --sites {}'.format(site)
+        command += ' --skip_copy'
+        call(command,shell=True)
+
+    # Download/Upload L2A, Calculate/Upload Preprocess
+    s2_data = os.path.join(TOPDIR,'Sentinel-2_Data',site)
+    command = python_path
